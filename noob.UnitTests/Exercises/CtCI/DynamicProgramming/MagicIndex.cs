86a034df392884103fa5717576d645e206235c8d
XEROINSIDE\ryan.achten <ryan.achten@xero.com>
2022-08-27 12:41:20 -0700

    test: add additional exercises

---
+++ noob.UnitTests/Exercises/CtCI/DynamicProgramming/MagicIndex.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Xunit;
+
+namespace noob.UnitTests.Exercises.CtCI.DynamicProgramming;
+
+/// <summary>
+/// A magic index in an array A [ 0 ••• n -1] is defined to be an index such that A[ i] =
+/// i.Given a sorted array of distinct integers, write a method to find a magic index, if one exists, in
+/// array A.
+/// </summary>
+public class MagicIndex
+{
+    [Theory]
+    [InlineData(new object[] { new int[] { 0 }, 0 })]
+    [InlineData(new object[] { new int[] { 1 }, -1 })]
+    [InlineData(new object[] { new int[] { 3, 3, 3, 3 }, 3 })]
+    public void WhenIndexMatchesValue_ThenMagicIndexIsReturned(int[] vals, int expectedResult)
+    {
+        var index1 = BruteForceFindMagicIndex(vals, 0);
+        var index2 = BinarySearchMagicIndex(vals, 0, vals.Length - 1);
+        Assert.Equal(expectedResult, index1);
+        Assert.Equal(expectedResult, index2);
+    }
+
+    private int BruteForceFindMagicIndex(int[] vals, int index)
+    {
+        if (vals[index] == index) return index;
+        if (index + 1 >= vals.Length) return -1;
+
+        return BruteForceFindMagicIndex(vals, index + 1);
+    }
+
+    private int BinarySearchMagicIndex(int[] vals, int start, int end)
+    {
+        if (end < start) return -1;
+
+        var midIndex = (start + end) / 2;
+        var midValue = vals[midIndex];
+        if (midValue == midIndex) return midIndex;
+
+        var leftIndex = Math.Min(midIndex - 1, midValue);
+        var leftValue = BinarySearchMagicIndex(vals, start, leftIndex);
+        if (leftValue >= 0)
+        {
+            return leftValue;
+        }
+
+        var rightIndex = Math.Max(midIndex + 1, midValue);
+        return BinarySearchMagicIndex(vals, rightIndex, end);
+    }
+}
---
+++ noob.UnitTests/Exercises/LeetCode/ZigzagConversion.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+using Xunit;
+
+namespace noob.UnitTests.Exercises.LeetCode;
+
+public class ZigzagConversion
+{
+    [Theory]
+    // P   A   H   N
+    // A P L S I I G
+    // Y   I R
+    [InlineData("PAYPALISHIRING", 3, "PAHNAPLSIIGYIR")]
+    // P I    N
+    // A   L S  I G
+    // Y A   H R
+    // P I
+    [InlineData("PAYPALISHIRING", 4, "PINALSIGYAHRPI")]
+    [InlineData("A", 1, "A")]
+    [InlineData("AB", 1, "AB")]
+    public void WhenConvertingArrayToString_ThenStringIsComputedUsingZigZagPattern(string str, int numRows, string expectedResult)
+    {
+        var result = Convert(str, numRows);
+        Assert.Equal(expectedResult, result);
+    }
+
+
+    // - iterate through through string, adding characters to row
+    // - when row = n, then we are on the next column
+    // - if column % n-1 == 0, then we're on another full column
+    // - otherwise, it's a single character entry
+    // - row index for single character entry = column % n-1
+    public string Convert(string s, int numRows)
+    {
+        // If there's only one row or the string is less than 2 characters, then we're good
+        if (numRows == 1 || s.Length < 2) return s;
+
+        var n = numRows - 1;
+        n = n < 0 ? 0 : n;
+
+        var builders = new StringBuilder[numRows];
+        for (var i = 0; i < numRows; i++)
+        {
+            builders[i] = new StringBuilder();
+        }
+
+        for (var i = 0; i < s.Length;)
+        {
+            if (i % n == 0)
+            {
+                // handle n row characters
+                // - compute row index based on j % numRows
+                for (var j = 0; j < numRows && j + i < s.Length; j++)
+                {
+                    var rowIndex = j % numRows;
+                    builders[rowIndex].Append(s[j + i]);
+                }
+                i += numRows;
+            }
+            else
+            {
+                // handle single character
+                var rowIndex = n - i % n;
+                builders[rowIndex].Append(s[i]);
+                i++;
+            }
+        }
+
+        var result = new StringBuilder();
+        foreach (var builder in builders) result.Append(builder.ToString());
+
+        return result.ToString();
+    }
+}
