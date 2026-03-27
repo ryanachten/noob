1a6c1091eabe60b2b4be44a14746b86c05d169fc
Ryan Achten <ryanachten@gmail.com>
2026-03-27 20:51:13 +1300

    feat: flesh out examples a bit more

---
+++ noob.Architecture/Hexagonal/Infrastructure/Adapters/Outbound/InMemoryOrderAdapter.cs
@@ -0,0 +1,14 @@
+namespace noob.Architecture.Hexagonal.Infrastructure.Adapters.Outbound;
+
+using noob.Architecture.Hexagonal.Application.Ports.Outbound;
+using noob.Architecture.Hexagonal.Domain;
+
+public class InMemoryOrderAdapter : IOrderOutputPort
+{
+    private readonly List<Order> _orders = new();
+
+    public void Save(Order order)
+    {
+        _orders.Add(order);
+    }
+}
---
+++ noob.Architecture/Onion/Infrastructure/Persistence/InMemoryOrderRepository.cs
@@ -0,0 +1,14 @@
+namespace noob.Architecture.Onion.Infrastructure.Persistence;
+
+using noob.Architecture.Onion.Domain.Entities;
+using noob.Architecture.Onion.Application.Interfaces;
+
+public class InMemoryOrderRepository : IOrderRepository
+{
+    private readonly List<Order> _orders = new();
+
+    public void Add(Order order)
+    {
+        _orders.Add(order);
+    }
+}
